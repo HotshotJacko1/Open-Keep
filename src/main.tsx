8ef54f373efd8287c61873f1ed1619cc1d11f595
[dyad] <hotshotjacko1@gmail.com>
2025-12-09 16:04:53 +0000

    [dyad] wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "./index.css";
+import "./globals.css"; // Changed from ./index.css to ./globals.css
 import { ThemeProvider } from "./context/theme-provider";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
