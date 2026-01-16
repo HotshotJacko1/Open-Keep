34655e0ab7a76b240d7a5d6ca23f6499e88e5fef
[dyad] <hotshotjacko1@gmail.com>
2026-01-16 11:39:15 +0000

    Updated src/components/SettingsDialog.tsx

--- src/components/SettingsDialog.tsx
+++ src/components/SettingsDialog.tsx
@@ -30,7 +30,7 @@ const SettingsDialog: React.FC<SettingsDialogProps> = ({ isOpen, onClose }) => {
   return (
     <>
       <Dialog open={isOpen} onOpenChange={onClose}>
-        <DialogContent className="sm:max-w-[425px] bg-background">
+        <DialogContent className="sm:max-w-[425px] bg-background text-primary">
           <DialogHeader>
             <DialogTitle>Settings</DialogTitle>
           </DialogHeader>
