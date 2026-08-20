cb22f420f79baaf53ef9313c83e2a0ce35aae9c8
Dyad <jackbarker635@gmail.com>
2026-08-20 13:21:56 +0100

    (1 files changed)

--- src/App.tsx
+++ src/App.tsx
@@ -247,7 +247,7 @@ const App = () => {
         <TooltipProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter future={{ v7_startTransition: true, v7_relativeSplatPath: true }}>
+          <BrowserRouter>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="*" element={<NotFound />} />
