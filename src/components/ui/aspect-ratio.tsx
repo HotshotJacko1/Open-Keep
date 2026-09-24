6a143527185e2850e8be5015f5ea9ca34e31ce51
Jack Barker <jackbarker635@gmail.com>
2026-09-24 09:04:03 +0100

    5.0.5

--- src/components/ui/aspect-ratio.tsx
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2026. Licensed under AGPLv3.
-import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
-
-const AspectRatio = AspectRatioPrimitive.Root;
-
-export { AspectRatio };
--- src/components/ui/use-toast.ts
+++
@@ -1,4 +0,0 @@
-// Copyright (c) 2026. Licensed under AGPLv3.
-import { useToast, toast } from "@/hooks/use-toast";
-
-export { useToast, toast };
